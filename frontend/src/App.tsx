e3b1fb956705de79f7cf4dcb98b4ecda183ac6af
Fikri <fikrizhanking@gmail.com>
2024-12-04 14:19:01 +0700

    [Add] Implemented Google Analytics v4 with VITE_GA_ID and tracking pageviews.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Home from "./layout/Home";
 import Earn from "./layout/Earn";
 import Friend from "./layout/Friend";
 import Wallet from "./layout/Wallet";
+import { useEffect } from "react";
 
 function App() {
+   ReactGA.initialize(import.meta.env.VITE_GA_ID);
+
+   const location = useLocation();
+   useEffect(() => {
+      ReactGA.send({
+         hitType: "pageview",
+         page: location.pathname + location.search,
+      });
+   }, [location]);
+
    return (
       <div className="p-4">
          <Routes>
